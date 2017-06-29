d927111e18d2e7ee4d7385c213eb866710f94b1d
Dane Petersen <thegreatsunra@gmail.com>
2017-06-29 15:58:41 -0500

    Organize server files and client files into folders under /src

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-app.use(express.static('public'))
+app.use(express.static(path.resolve(__dirname, '../client')))
 
 app.listen(3000, () => {
   console.log('Express listening on port 3000')
