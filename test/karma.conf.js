772766df36cd384b97faf5f06a28c2da175730b2
Dane Petersen <thegreatsunra@gmail.com>
2017-06-29 13:59:25 -0500

    Add phantomJS to run browser tests headlessly

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,9 +1,10 @@
 module.exports = (config) => {
   config.set({
+    browsers: ['PhantomJS'],
     frameworks: ['mocha'],
     files: [
       '*.js'
     ],
-    plugins: ['karma-mocha'],
+    plugins: ['karma-mocha', 'karma-phantomjs-launcher'],
   })
 }
