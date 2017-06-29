8e3cd5467f715368933c9ed790e6af6f8aab7dd5
Dane Petersen <thegreatsunra@gmail.com>
2017-06-29 15:12:14 -0500

    Add nightwatch dependencies and e2e test

---
+++ test/e2e/specs/test.js
@@ -0,0 +1,20 @@
+// For authoring Nightwatch tests, see
+// http://nightwatchjs.org/guide#usage
+
+module.exports = {
+  'default e2e tests': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.title')
+      .assert.containsText('.title', 'Your basic express server is working!')
+      .assert.elementPresent('.description')
+      .assert.containsText('.description', 'Now get out there and build something great!')
+      .end()
+  }
+}
