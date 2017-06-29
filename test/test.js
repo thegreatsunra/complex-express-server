c876baca0f04b4ffac23fc3d95f1a17f489c7853
Dane Petersen <thegreatsunra@gmail.com>
2017-06-29 14:04:14 -0500

    Ignore describes and its that go undefined

--- test/test.js
+++ test/test.js
@@ -1,7 +1,9 @@
 const assert = require('chai').assert
+/* eslint-disable no-undef */
 describe('Array', () => {
   describe('#indexOf()', () => {
     it('should return -1 when the value is not present', () => {
       assert.equal(-1, [1, 2, 3].indexOf(4))
     })
   })
+/* eslint-enable */
