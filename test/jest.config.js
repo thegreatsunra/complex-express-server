2b08cf72578884255bdac5eabfc7b571a1d8549a
Dane Petersen <thegreatsunra@gmail.com>
2017-06-30 10:39:37 -0500

    Replace karma, mocha, phantomjs with jest
    Add file and style mocks to make jest play nicely with webpack

---
+++ test/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  "moduleNameMapper": {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/test/client/__mocks__/fileMock.js",
+    "\\.(css|scss)$": "<rootDir>/test/client/__mocks__/styleMock.js"
+  }
+}
